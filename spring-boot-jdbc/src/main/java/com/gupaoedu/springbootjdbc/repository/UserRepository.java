2704de369982f56f57efc6118564d7cddcda00fa
miaoo92 <914983053@qq.com>
2018-04-16 00:09:56 +0800

    事务控制
    三种控制事务的方式：
    1.关闭自动提交
    2.添加Transactional注解
    3.platformTransactionManager的方式

--- spring-boot-jdbc/src/main/java/com/gupaoedu/springbootjdbc/repository/UserRepository.java
+++ spring-boot-jdbc/src/main/java/com/gupaoedu/springbootjdbc/repository/UserRepository.java
@@ -4,9 +4,19 @@ package com.gupaoedu.springbootjdbc.repository;
 import com.gupaoedu.springbootjdbc.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -29,18 +39,81 @@ public class UserRepository {
     @Qualifier("slaveDataSource")
     private final DataSource slaveDataSource;
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    private final JdbcTemplate jdbcTemplate;
+
+    private final PlatformTransactionManager platformTransactionManager;
+
     @Autowired
-    public UserRepository(DataSource dataSource, DataSource materDataSource, DataSource slaveDataSource) {
+    public UserRepository(DataSource dataSource, DataSource materDataSource,
+                          DataSource slaveDataSource,
+                          JdbcTemplate jdbcTemplate,
+                          PlatformTransactionManager platformTransactionManager) {
         this.dataSource = dataSource;
         this.materDataSource = materDataSource;
         this.slaveDataSource = slaveDataSource;
+        this.jdbcTemplate = jdbcTemplate;
+        this.platformTransactionManager = platformTransactionManager;
+    }
+
+    @Transactional
+    public boolean transactionSave(User user){
+        Boolean success = false;
+        success = jdbcTemplate.execute("INSERT INTO  user(name) VALUE (?)", new PreparedStatementCallback<Boolean>() {
+            @Override
+            public Boolean doInPreparedStatement(PreparedStatement preparedStatement) throws SQLException, DataAccessException {
+                preparedStatement.setString(1,user.getName());
+                return preparedStatement.executeUpdate() > 0;
+            }
+        });
+
+        return success;
     }
 
 
     public boolean save(User user){
+        Boolean success = false;
+
+        DefaultTransactionDefinition defaultTransactionDefinition = new DefaultTransactionDefinition();
+        TransactionStatus transactionStatus = platformTransactionManager.getTransaction(defaultTransactionDefinition);
+        success = jdbcTemplate.execute("INSERT INTO  user(name) VALUE (?)", new PreparedStatementCallback<Boolean>() {
+            @Override
+            public Boolean doInPreparedStatement(PreparedStatement preparedStatement) throws SQLException, DataAccessException {
+                preparedStatement.setString(1,user.getName());
+                return preparedStatement.executeUpdate() > 0;
+            }
+        });
+        platformTransactionManager.commit(transactionStatus);
+        return success;
+    }
+
+
+    public boolean jdbcSave(User user) throws SQLException {
         System.out.printf("[Thread : %s ]save User : %s \n",Thread.currentThread().getName(),user);
-        return true;
+        Boolean success = false;
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            connection.setSavepoint("T1");   //安全点的方式来控制事务
+            connection.setAutoCommit(false);        //关闭自动提交的方式来控制事务
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO  user(name) VALUE (?)");
+            preparedStatement.setString(1,user.getName());
+            success = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            connection.rollback();
+        }finally {
+            if(connection != null){
+                try {
+                    connection.commit();
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return success;
     }
 
     public Collection<User> findAll(){
